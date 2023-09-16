15beb24dbf3257e76e1db286277d2a5c173f487c
Anuj <tyagianuj234@gmail.com>
2023-09-16 12:45:48 +0530

    box3 complete

---
+++ script.js
@@ -0,0 +1,29 @@
+const squareElements = document.querySelectorAll('.square');
+let myArray = [];
+let index = 0;
+
+squareElements.forEach((square) => {
+  square.addEventListener('click', () => {
+    square.style.backgroundColor = 'red';
+    const squareId = square.id;
+    myArray.push(squareId);
+
+    if (squareId == 9) {
+      setTimeout(() => {
+        changeColor();
+      }, 500);
+    }
+  });
+});
+
+function changeColor() {
+  if (index < myArray.length) {
+    const elementId = document.getElementById(myArray[index]);
+    elementId.style.backgroundColor = 'rgb(0, 0, 159)';
+    index++;
+
+    setTimeout(() => {
+      changeColor();
+    }, 500);
+  }
+}
